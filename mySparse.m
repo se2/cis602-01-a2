ba49bc3df2f4eda40347de01d9babd63e41615bf
Tu Luong <luongduc.tu1992@gmail.com>
2017-03-24 16:41:44 -0400

    Finish Q3

--- mySparse.m
+++ mySparse.m
@@ -1,7 +1,6 @@
-load('ORL.mat')
+load('ORL.mat');
 
 % call LDA
-options = [];
 options.Fisherface = 1;
 [eigVector, eigValue] = LDA(gnd, options, fea);
 fea = fea * eigVector;
@@ -9,13 +8,27 @@ fea = fea * eigVector;
 % normalize data
 fea = NormalizeFea(fea);
 
-% compute B
-trainData = fea(1:5,:);
-testSample = fea(6:10,:);
-[nS, nFea] = size(trainData);
+% size of fea
+[nS, nFea] = size(fea);
 
-B = zeros(nS,1);
-
+% compute beta and residual
+R = zeros(nS/10,1);
+B = zeros(nS/2,1);
+bi = 1;
+ri = 1;
+r = 5;
 for i=1:nS
-   subB = lasso(trainData(i,:), testSample(i,:));
+    % step size is 10
+    if (rem(i, 10) == 1)
+        trainData = (fea(i:(i+4),:))';
+        testSample = (fea((i+5):(i+9),:))';
+        B(bi:(bi+r-1),:) = lasso(trainData, testSample(:,1), 'lambda', .1);
+        R(ri,:) = norm(trainData * B(bi:(bi+r-1),:) - testSample(:,1));
+        bi = bi + r;
+        ri = ri + 1;
+    end
 end
+
+% min value and index of min value
+[vMin, iMin] = min(R);
+
