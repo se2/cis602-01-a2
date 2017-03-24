085a5b7a2ed3a71ed43aeba783ba7703582b5d72
Tu Luong <luongduc.tu1992@gmail.com>
2017-03-23 20:46:23 -0400

    Optimize code

--- myLDA.m
+++ myLDA.m
@@ -1,40 +1,34 @@
-function [eigvector, eigvalue] = lda(trainLabel, trainFea)
+function [eigvector, eigvalue] = myLDA(trainLabel, trainFea)
 
-% call myPCA to reduce dimension
-[eigvectorPCA, eigvaluePCA] = myPCA(trainFea);
-trainFea = trainFea * eigvectorPCA; 
+    % call myPCA to reduce dimension
+    [eigvectorPCA, eigvaluePCA] = myPCA(trainFea);
+    trainFea = trainFea * eigvectorPCA;
 
-% normalize data
-trainFea = NormalizeFea(trainFea);
+    % normalize data
+    trainFea = NormalizeFea(trainFea);
 
-[nSmp, nFea] = size(trainFea);
+    classLabel = unique(trainLabel);
+    nClass = length(classLabel);
 
-classLabel = unique(trainLabel);
-nClass = length(classLabel);
+    Sw = zeros();
+    for i=1:nClass
+        index = find(trainLabel == classLabel(i));
+        classMean = mean(trainFea(index,:), 1);
+        Sw = Sw + (trainFea(index,:) - classMean)' * (trainFea(index,:) - classMean);
+    end
 
-sampleMean = mean(trainFea, 1);
-trainFea = (trainFea - repmat(sampleMean, nSmp, 1));
+    centerData = mean(trainFea);
 
-Sw = zeros();
+    Sb = zeros();
+    for i=1:nClass
 
-for i=1:nClass
-    index = find(trainLabel == classLabel(i));
-    classMean = mean(trainFea(index,:), 1);
-    Sw = Sw + (trainFea(index,:) - classMean)' * (trainFea(index,:) - classMean);
+        index = find(trainLabel == classLabel(i));
+        classMean = mean(trainFea(index,:),1);
+        [ni, nf] = size(trainFea(index,:));
+        Sb = Sb + ni * (classMean - centerData)' * (classMean - centerData);
+    end
+
+    [eigvector, eigvalue] = eig(Sb, Sw);
+
 end
-
-centerData = mean(trainFea);
-
-Sb = zeros();
-
-for i=1:nClass
-    
-    index = find(trainLabel == classLabel(i));
-    classMean = mean(trainFea(index,:),1);
-    [ni, nf] = size(trainFea(index,:));
-    Sb = Sb + ni * (classMean - centerData)' * (classMean - centerData);
-end
-
-[eigvector, eigvalue] = eig(Sb, Sw);
-
 
--- myPCA.m
+++ myPCA.m
@@ -1,29 +1,17 @@
-function [eigvector, eigvalue] = pca(data)
+function [eigvector, eigvalue] = myPCA(data)
 
-% center data
-mn = mean(data);
-datacenter = bsxfun(@minus, data, mn);
+    % center data
+    mn = mean(data);
+    datacenter = bsxfun(@minus, data, mn);
 
-%Normalize data
-datacenter = NormalizeFea(datacenter);
+    %Normalize data
+    datacenter = NormalizeFea(datacenter);
 
-% calculate the covariance matrix
-covariance = (datacenter' * datacenter);
-% covariance = cov(data);
+    % calculate the covariance matrix
+    covariance = (datacenter' * datacenter);
+    % covariance = cov(data);
 
-% calculate the eigenvectors and eigenvalues 
-[eigvector, eigvalue] = eig(covariance);
+    % calculate the eigenvectors and eigenvalues 
+    [eigvector, eigvalue] = eig(covariance);
 
-eigvalue = diag(eigvalue);
-        
-[dump, index] = sort(-eigvalue);
-eigvalue = eigvalue(index);
-eigvector = eigvector(:, index);
-
-maxEigValue = max(abs(eigvalue));
-eigIdx = find(abs(eigvalue)/maxEigValue < 1e-10);
-eigvalue(eigIdx) = [];
-eigvector(:,eigIdx) = [];
-
-% compute egivector
-% eigvector = eigvector(:,1:reduceddim)
+end
