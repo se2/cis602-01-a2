c7a60ffb579b1feb2db9976359063e9a68f5f686
Tu Luong <luongduc.tu1992@gmail.com>
2017-03-26 17:21:01 -0400

    Refactor mySparse.m

--- mySparse.m
+++ mySparse.m
@@ -1,26 +1,10 @@
 load('ORL.mat');
 
-% % reduce dim of data
-
-% % call LDA
-% options.Fisherface = 1;
-% [eigVector, eigValue] = LDA(gnd, options, fea);
-
-% fea = fea * eigVector;
-% 
-% call PCA
-% [eigVector, eigValue] = PCA(fea);
-% fea = fea * eigVector;
-
-% applying PCA first
+% applying PCA
 options=[];
 options.ReducedDim=100;
 [eigvectorPCA, eigvaluePCA] = PCA(fea,options);
 fea = fea * eigvectorPCA;
-
-% normalize data
-fea = NormalizeFea(fea);
-
 
 % size of fea
 [nS, nFea] = size(fea);
@@ -55,16 +39,16 @@ R = [];
 label = [];
 
 for i=1:200
-%     compute B for testSample(i)
-    B = lasso( trainData, testSample(:,i), 'lambda', .015);
+    % compute B for testSample(i)
+    B = lasso( trainData, testSample(:,i), 'Lambda', .001);
     for j=1:200
         R(j,:) = norm( trainData(:,j) * B(j,:) - testSample(:,i));   
     end
-%     get min residual and index of min residual
+    % get min residual and index of min residual
     [vMin, iMin] = min(R);
-%     round up to be label
+    % round up to be label
     iMin = ceil(iMin/5);
-%     store label for testSample(i)
+    % store label for testSample(i)
     label(i,:) = iMin;
 end
 
